f0cdb16819cc477a381aeaf98f47d3750536208c
Nikita Fediuchin <cfnptr@gmail.com>
2024-10-29 23:20:21 +0200

    Initial commit

---
+++ source/main.cpp
@@ -0,0 +1,123 @@
+// Copyright 2024-2024 Nikita Fediuchin.All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "garden/main.hpp"
+#include "garden/system/log.hpp"
+#include "garden/system/link.hpp"
+#include "garden/system/camera.hpp"
+#include "garden/system/spawner.hpp"
+#include "garden/system/settings.hpp"
+#include "garden/system/resource.hpp"
+#include "garden/system/transform.hpp"
+#include "garden/system/animation.hpp"
+#include "garden/system/character.hpp"
+#include "garden/system/fpv-controller.hpp"
+#include "garden/system/render/mesh.hpp"
+#include "garden/system/render/fxaa.hpp"
+#include "garden/system/render/bloom.hpp"
+#include "garden/system/render/skybox.hpp"
+#include "garden/system/render/deferred.hpp"
+#include "garden/system/render/pbr-lighting.hpp"
+#include "garden/system/render/tone-mapping.hpp"
+#include "garden/system/render/auto-exposure.hpp"
+
+#include "demo/app-defines.hpp"
+#include "demo/system/main.hpp"
+
+#if GARDEN_EDITOR
+#include "garden/editor/system/log.hpp"
+#include "garden/editor/system/ecs.hpp"
+#include "garden/editor/system/link.hpp"
+#include "garden/editor/system/camera.hpp"
+#include "garden/editor/system/spawner.hpp"
+#include "garden/editor/system/physics.hpp"
+#include "garden/editor/system/graphics.hpp"
+#include "garden/editor/system/transform.hpp"
+#include "garden/editor/system/hierarchy.hpp"
+#include "garden/editor/system/animation.hpp"
+#include "garden/editor/system/render/bloom.hpp"
+#include "garden/editor/system/render/skybox.hpp"
+#include "garden/editor/system/render/deferred.hpp"
+#include "garden/editor/system/render/pbr-lighting.hpp"
+#include "garden/editor/system/render/tone-mapping.hpp"
+#include "garden/editor/system/render/auto-exposure.hpp"
+#include "garden/editor/system/render/mesh-gizmos.hpp"
+#include "garden/editor/system/render/mesh-selector.hpp"
+#include "garden/editor/system/render/gpu-resource.hpp"
+#include "garden/editor/system/render/infinite-grid.hpp"
+#endif
+
+using namespace garden;
+using namespace garden::demo;
+
+static void entryPoint()
+{
+	auto manager = new Manager();
+	createAppSystem(manager);
+	manager->createSystem<MainSystem>();
+	manager->createSystem<DoNotDestroySystem>();
+	manager->createSystem<DoNotDuplicateSystem>();
+	manager->createSystem<DoNotSerializeSystem>();
+	manager->createSystem<LogSystem>();
+	manager->createSystem<SettingsSystem>();
+	manager->createSystem<ResourceSystem>();
+	manager->createSystem<LinkSystem>();
+	manager->createSystem<SpawnerSystem>();
+	manager->createSystem<CameraSystem>();
+	manager->createSystem<FpvControllerSystem>();
+	manager->createSystem<TransformSystem>();
+	manager->createSystem<InputSystem>();
+	manager->createSystem<AnimationSystem>();
+	manager->createSystem<PhysicsSystem>();
+	manager->createSystem<CharacterSystem>();
+	manager->createSystem<GraphicsSystem>();
+	manager->createSystem<DeferredRenderSystem>();
+	manager->createSystem<SkyboxRenderSystem>();
+	manager->createSystem<MeshRenderSystem>();
+	manager->createSystem<PbrLightingRenderSystem>();
+	manager->createSystem<BloomRenderSystem>();
+	manager->createSystem<ToneMappingRenderSystem>();
+	manager->createSystem<AutoExposureRenderSystem>();
+	manager->createSystem<FxaaRenderSystem>();
+	manager->createSystem<ThreadSystem>();
+
+	#if GARDEN_EDITOR
+	manager->createSystem<EditorRenderSystem>();
+	manager->createSystem<HierarchyEditorSystem>();
+	manager->createSystem<EcsEditorSystem>();
+	manager->createSystem<LogEditorSystem>();
+	manager->createSystem<LinkEditorSystem>();
+	manager->createSystem<SpawnerEditorSystem>();
+	manager->createSystem<CameraEditorSystem>();
+	manager->createSystem<TransformEditorSystem>();
+	manager->createSystem<AnimationEditorSystem>();
+	manager->createSystem<PhysicsEditorSystem>();
+	manager->createSystem<GraphicsEditorSystem>();
+	manager->createSystem<GpuResourceEditorSystem>();
+	manager->createSystem<InfiniteGridEditorSystem>();
+	manager->createSystem<MeshSelectorEditorSystem>();
+	manager->createSystem<MeshGizmosEditorSystem>();
+	manager->createSystem<DeferredRenderEditorSystem>();
+	manager->createSystem<PbrLightingRenderEditorSystem>();
+	manager->createSystem<BloomRenderEditorSystem>();
+	manager->createSystem<ToneMappingRenderEditorSystem>();
+	manager->createSystem<AutoExposureRenderEditorSystem>();
+	#endif
+
+	manager->initialize();
+	manager->start();
+	delete manager;
+}
+
+GARDEN_DECLARE_MAIN(entryPoint)
---
+++ source/system/demo.cpp
@@ -0,0 +1,70 @@
+// Copyright 2024-2024 Nikita Fediuchin.All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "demo/system/main.hpp"
+#include "garden/system/graphics.hpp"
+#include "garden/system/render/skybox.hpp"
+#include "garden/system/render/pbr-lighting.hpp"
+
+using namespace garden::demo;
+
+constexpr auto skyboxPath = "cubemaps/kloofendal";
+
+MainSystem::MainSystem()
+{
+	ECSM_SUBSCRIBE_TO_EVENT("PostInit", MainSystem::postInit);
+	ECSM_SUBSCRIBE_TO_EVENT("Update", MainSystem::update);
+}
+MainSystem::~MainSystem()
+{
+	if (Manager::Instance::get()->isRunning())
+	{
+		ECSM_UNSUBSCRIBE_FROM_EVENT("PostInit", MainSystem::postInit);
+		ECSM_UNSUBSCRIBE_FROM_EVENT("Update", MainSystem::update);
+	}
+
+	unsetSingleton();
+}
+
+void MainSystem::postInit()
+{
+	auto manager = Manager::Instance::get();
+	auto graphicsSystem = GraphicsSystem::Instance::get();
+	graphicsSystem->setWindowTitle("Voxfield");
+
+	
+	auto pbrLightingView = manager->add<PbrLightingRenderComponent>(graphicsSystem->camera);
+	PbrLightingRenderSystem::Instance::get()->loadCubemap(skyboxPath,
+		pbrLightingView->cubemap, pbrLightingView->sh, pbrLightingView->specular);
+	manager->add<SkyboxRenderComponent>(graphicsSystem->camera);
+}
+void MainSystem::update()
+{
+	auto graphicsSystem = GraphicsSystem::Instance::get();
+	auto skyboxSystem = SkyboxRenderSystem::Instance::get();
+	auto skyboxView = skyboxSystem->getComponent(graphicsSystem->camera);
+
+	if (!skyboxView->descriptorSet)
+	{
+		auto skyboxPipeline = graphicsSystem->get(skyboxSystem->getPipeline());
+		if (skyboxPipeline->isLoaded())
+		{
+			auto pbrLightngSystem = PbrLightingRenderSystem::Instance::get();
+			auto pbrLightingView = pbrLightngSystem->getComponent(graphicsSystem->camera);
+			skyboxView->cubemap = pbrLightingView->cubemap;
+			skyboxView->descriptorSet = skyboxSystem->createSharedDS(
+				skyboxPath, ID<Image>(pbrLightingView->cubemap));
+		}
+	}
+}
